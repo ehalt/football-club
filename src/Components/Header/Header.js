0bb15cd276256c5f6407453ebd220b1c40d9c5de
ehalt <torikussadikk@gmail.com>
2021-03-01 23:16:38 -0800

    tor

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Header.css';
+const Header = () => {
+    return (
+        <div className='header container my-4'>
+            <nav className='nav text-center'>
+                 <h3 className="logo-name">Football club</h3>
+            </nav>
+        </div>
+    )
+}
+
+export default Header
---
+++ src/Components/Player/Player.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+import './Player.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faCheck, faPlus } from '@fortawesome/free-solid-svg-icons'
+const Player = ({player, selectPlayer}) => {
+    const {img, name , age, salary} = player ; 
+    const [isSelect , setIsSelect] = useState(false)
+
+    return (
+        <div className='container'>
+            <div className="single-player-card text-center">
+                <img className='player-img' src={img} alt={img} />
+                <div className="more-info">
+                    <div className="d-flex justify-content-between">
+                    <h4>Name: {name}</h4>
+                    <h5>Age: {age}</h5>
+                    </div>
+                    
+                    <h4 className='price-info'>Salary : ${salary}</h4>
+
+                    <button disabled={isSelect} onClick = {() => {
+                        selectPlayer(player)
+                        setIsSelect(true)
+                    }} className="btn btn-block"> {isSelect ?<FontAwesomeIcon className='mr-1' icon={faCheck} /> : <FontAwesomeIcon className='mr-1' icon={faPlus} />}
+                     {isSelect ? "Selected" : 'Select player'}</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Player
